f4334d2cee4ea83fb9dc2a706debc99f475fabac
DonnieTu <donnie.tu@autodesk.com>
2017-10-09 21:55:32 +0800

    5.rb

---
+++ 05.rb
@@ -0,0 +1,16 @@
+name="Donnie Tu"
+age=40
+weight=180 #lbs
+height=74 #inches
+eyes="black"
+teeth="white"
+hair="black"
+
+puts "Let's talk about #{name}."
+puts "He's #{height*2.54} cm tall."
+puts "He's #{weight*0.45359237} kg heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are ususally #{teeth} depending on the coffee."
+
+puts "If I add #{age},#{height}, and #{weight} I get #{ age+height+weight}."

464acbfe6a27faf943a05b604c123d2461445144
Donnie Tu <Donnie.Tu@autodesk.com>
2017-10-11 17:00:11 +0800

    09

---
+++ 09.rb
@@ -0,0 +1,14 @@
+days="Mon Tue Wed Thu Fri Sat Sun"
+months="Jan\nFeb\nMar\nApr\nMay"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts "I'm \\ a \\ cat"
+
+puts """
+\tThere'ss something going on here.
+\tWith the three double-quotes.
+\tWe'll be able to type as much as we like.
+\tEven 4 lines if we want, or 5, or 6.
+"""

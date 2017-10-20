baa140adeb6675770977894796dd20841aded5b5
DonnieTu <donnie.tu@autodesk.com>
2017-10-20 20:27:54 +0800

    Lexicon with testing

--- projects/Ex49/lib/lexicon.rb
+++ projects/Ex49/lib/lexicon.rb
@@ -1,27 +1,51 @@
 class Lexicon
-    @@verbs=['go','verb']
-    @@direction=['north','west','east','west','down','up','left','right','back']
+    @@verbs=['go','kill','eat','stop']
+    @@directions=['north','west','east','south','down','up','left','right','back']
     @@nouns=['door','bear','princess','cabinet']
-    @@number=[0..9]
-    @@stop_words=['the','in','of','from','at','it']
+    @@stops=['the','in','of','from','at','it']
 
-    @@directory={'verbs'=>@@verbs,
-    'direction'=>@@direction,
-    'vouns'=>@@nouns,
-    'number'=>@@number,
-    'stop_words'=>@@stop_words
+    @@directory={
+        'direction'=>@@directions,
+        'verb'=>@@verbs,
+        'noun'=>@@nouns,
+        'number'=>nil,
+        'stop'=>@@stops
     }
 
+    def Lexicon.convert_number(object)
+        begin
+            return Integer(object)
+        rescue
+            return nil
+        end
+    end
+
     def Lexicon.scan(input)
-        input.downcase.split(' ').each do |word|
-            result=[]
+        result=[]
+        input.split(' ').each do |object|
+            word=object.downcase
+            found=false
             for kv in @@directory
-                if(kv[1].find_index(word))
-                     result.push([kv[0],word])
-                     break
+                if(kv[0]!='number')
+                    if (kv[1].find_index(word))
+                        result.push([kv[0],object])
+                        found=true
+                    end
+                else
+                    number=convert_number(word)
+                    if(number)
+                        result.push([kv[0],number])
+                        found=true
+                    end
+                end
+                if(found) 
+                    break
                 end
             end
-            return result
+            if(!found)
+                result.push(['error',object])
+            end
         end
+        return result
     end
 end
--- projects/Ex49/tests/test_lexicon.rb
+++ projects/Ex49/tests/test_lexicon.rb
@@ -8,6 +8,70 @@ class TestLexicon<Minitest::Test
     end
     
     def test_directions()
-        assert_equal(Lexicon.scan("north"),[['direction','north']])
+        assert_equal([['direction','north']],Lexicon.scan("north"))
+        assert_equal(
+        [
+            ['direction','north'],
+            ['direction','south'],
+            ['direction','east']
+        ],
+        Lexicon.scan("north south east"))
     end
+
+    def test_verbs()
+        assert_equal([['verb','go']],Lexicon.scan("go"))
+        assert_equal(
+        [
+            ['verb','go'],
+            ['verb','kill'],
+            ['verb','eat']
+        ],
+        Lexicon.scan("go kill eat"))
+    end
+
+    def test_stops()
+        assert_equal([['stop','the']],Lexicon.scan("the"))
+        assert_equal(
+        [
+            ['stop','the'],
+            ['stop','in'],
+            ['stop','of']
+        ],
+        Lexicon.scan("the in of"))
+    end
+
+    def test_nouns()
+        assert_equal([['noun','bear']],Lexicon.scan("bear"))
+        assert_equal(
+        [
+            ['noun','bear'],
+            ['noun','princess']
+        ],
+        Lexicon.scan("bear princess"))
+    end
+
+    def test_numbers()
+        assert_equal([['number',1234]],Lexicon.scan("1234"))
+        assert_equal(
+        [
+            ['number',3],
+            ['number',91223]
+        ],
+        Lexicon.scan("3 91223"))
+    end
+
+    def test_errors()
+        assert_equal(Lexicon.scan("ADFD"),[['error',"ADFD"]])
+        assert_equal(
+        [
+            ['noun','bear'],
+            ['error','IAS'],
+            ['noun','princess']
+        ],
+        Lexicon.scan("bear IAS princess"))
+    end
+
+
+
+
 end
